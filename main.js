e830242b96055a43489a3aae2ec53063969893d5
Harsh <harshshetty422@gmail.com>
2021-11-09 09:59:03 +0530

    About let, const, 5 Data types,Concatenation and Template strings

--- main.js
+++ main.js
@@ -5,3 +5,51 @@ console.log('Hello World');
 console.error('Hello World');
 console.warn('Hello World');
 */
+
+//let, const
+
+/*let age = 30;
+age = 13;
+console.log(age)*/
+
+/*const age = 13;
+//const: constant
+console.log(age);*/
+
+/*let score;
+score = 10;
+console.log(score);*/
+
+// We can't reassign const and score can't be reinitialized
+/*const score;
+score = 10;
+console.log(score);*/
+
+// 5 Basic DATATYPES
+// String, Number, Boolean, Undefined, Null
+
+/*const name = 'Harshavardhan';
+const age = 13;
+const height = 5.5;
+const isCool = true;
+const x = null;//typeof null evaluates to 'object'
+const y = undefined;
+let z;//not defined(not assigned the value)*/
+
+/*console.log(typeof name);
+console.log(typeof age);
+console.log(typeof height);
+console.log(typeof isCool);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);*/
+
+/*const name = 'Harshavardhan'
+const age = '13'
+
+//Concatenation
+console.log('My name is ' + name + ' and I am ' + age)//old method
+//Template
+console.log(`My name is ${name} and I am ${age}`)
+const hello = `My name is ${name} and I am ${age}`
+console.log(hello)*/
